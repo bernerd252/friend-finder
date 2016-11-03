b16d9408897d423efc16a7b807985ba619e041b8
Eedouard Batlle <Eedouard Batlle>
2016-11-03 15:39:07 -0400

    logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,6 +14,38 @@ var friends = [
      4,
      1
    ]
+},
+{
+  "name":"Ahmed2",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+     5,
+     5,
+     5,
+     5,
+     5,
+     5,
+     5,
+     5,
+     5,
+     5,
+   ]
+},
+{
+  "name":"Ahmed3",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+     1,
+     1,
+     1,
+     1,
+     1,
+     1,
+     1,
+     1,
+     1,
+     1
+   ]
 }
 ];
  module.exports = friends;
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -5,45 +5,54 @@ var path 			= require('path');
 // (ex:localhost:PORT/api/admin...they are shown a JSON of the data in the table) 
 
 var totalDifference = 0;
-
+var match
 module.exports = function(app){
 	app.get('/api/friends', function(req, res){
 		res.json(friendData);
-	});
 
+	});
+console.log(friendData)
 //API POST Request-handles when user submits a form & thus submits data to the server.
 // In each of the below cases, when a user submits form data (a JSON object)
 // ...the JSON is pushed to the appropriate Javascript array
 
 
-	// app.post('/api/friends', function(req, res){
-
-	// 	var newUser = req.body;
- //        var diffs = [];
- //        if (friendsData.length < 1) {
- //            console.log("unable to do calculation; not enough users");
- //        } else {
- //            compareFriends(friendsData, newUser, diffs);
- //            var lowest = diffs[0];
- //            for (var i = 0; i < diffs.length; i++) {
- //                if (diffs[i] < lowest) {
- //                    lowest = diffs[i];
- //                }
- //            };
- //            var bestMatch = diffs.indexOf(lowest);
- //            res.send(friendsData[bestMatch]);
- //        };
- //        friendsData.push(newUser);
-	// });
-
-// 	function compareFriends(friends, newUser, diffs) {
-// 	    var curUserIndex = 0;
-// 	    while (curUserIndex < friends.length) {
-// 	        var totalDifference = 0;
-// 	        for (var i = 0; i < newUser.scores.length; i++) {
-// 	            totalDifference += Math.abs(parseInt(friends[curUserIndex].scores[i]) - parseInt(newUser.scores[i]));
-// 	        }
-// 	        diffs.push(totalDifference);
-// 	        curUserIndex++;
-// 	    }
+	app.post('/api/friends', function(req, res){
+	var newUser = req.body;
+	var sum = 0;
+	var possibleMatchScore = 100;
+	var possibleMatch = [];
+	
+	for( var i = 0; i < newUser.scores.length; i++ ){
+	    sum += parseInt( newUser.scores[i], 10 ); //don't forget to add the base
 	}
+	// var newUserScore = sum += parseInt(newUser.scores, 10 );
+	// 
+	var newUserScore = sum;
+    
+
+    for (var i = 0; i<friendData.length; i++){
+       var friendScores = friendData[i].scores
+       // 
+       summ = 0
+       for( var x = 0; x < newUser.scores.length; x++ ){
+       summ += parseInt( friendScores[x], 10 );}
+       // for (var j = 0; j<friendScores.length; j++){
+       // 		sum += parseInt( friendScores[i], 10 ); 
+       // console.log(summ);
+       var totalDifference = sum - summ
+       parseInt(totalDifference);
+       Math.abs(totalDifference);
+       console.log(totalDifference)     
+       
+       if (totalDifference<possibleMatchScore){
+       	possibleMatchScore = totalDifference;
+       	possibleMatch = friendData[i];
+       	console.log(possibleMatch)
+       }
+
+   	}
+ });
+}
+	
+
